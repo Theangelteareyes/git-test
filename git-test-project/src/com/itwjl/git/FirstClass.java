2b0a52cedd7cc592a840fcbaf770efe361923e8d
liuzhibi <liuzhibi@qq.com>
2019-11-15 09:59:54 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itwjl/git/FirstClass.java
@@ -0,0 +1,17 @@
+package com.itwjl.git;
+
+/**
+ * @program: git-test-project
+ * @description:
+ * @author: Mr.Wang
+ * @create: 2019-11-15 09:53
+ **/
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
